d8a448f75eb877832b088a52e9b51970ca192564
neuro <neuro@well.com>
2013-11-20 22:59:22 +0000

    First cut leo 4.0 with content from 2.0

---
+++ web/current/guidelines.php
@@ -0,0 +1,52 @@
+<?php
+// lists.evolt.org
+// Copyright (c) 1999-2013 evolt.org, William Anderson
+?>
+<?php
+include "assets/inc/00-functions.inc";
+$site_name = "lists.evolt.org";
+$page_title = "thelist Information &amp; Guidelines";
+$short_title = "Info &amp; Guidelines";
+$page_type = "page";
+$upstairs_uri = "/";
+$upstairs_title = "Mailing Lists";
+include "assets/inc/10-header.inc";
+include "assets/inc/20-message.inc";
+?>
+<div class="wrapper">
+    <section id="content" class="full-width" role="main">
+    	<div class="p100">
+    		<h2><strong>thelist</strong> Information &amp; Guidelines</h2>
+    		<h3>General Information</h3>
+			<p>The list is intended for <em>professional</em> Web workers: designers, webmasters, programmers, what have you. Anything that relates to Web development is up for discussion.</p>
+			<p>The list is unmoderated (though monitored), and is maintained and supported by volunteers.</p>
+			<p>Individuals who post to the list hold the copyright to their own posts. </p>
+			<h3>Discussion List Guidelines</h2>
+		</div>
+		<ol>
+			<li>Respect each other. Diversity in Web development is not only tolerated but encouraged. Argument and debate are fine. Character digs and personal attacks are not. You are responsible for what you write. Those who cannot be civil will be removed from the list of subscribers and will not be permitted to return.</li>
+			<li>Limited off-topic posts are fine; however they must be labelled [OT] in the subject line. Also, we ask that you "pay" for off-topic posts by including a Web tip that you find useful. Example:
+<pre>    &lt;tip&gt;
+    Include META tags in all of your pages for better search engine placement.
+    &lt;/tip&gt;</pre>
+			Please stay consistent with the <code>&lt;tip&gt;&lt;/tip&gt;</code> tags, so that our <strong>Tip Harvester</strong> can extract them for the archive. If you want to be extra helpful, your opening tag could be formatted:
+<pre>    &lt;tip type=&quot;META Tags&quot; author=&quot;Your Name&quot;&gt;
+    Include META tags in all of your pages for better search engine placement.
+    &lt;/tip&gt;</pre></li>
+			<li>To the best of your ability, keep posts short.</li>
+			<li>Keep your subject lines specific, as many people decide what posts to read from the subject line. "Help!," "What am I doing wrong?,"and "Arrraugh" are not acceptable subject lines.</li>
+			<li>If an ongoing discussion drifts from the initial subject, please change the subject line to reflect the new subject. For example, "e-commerce solutions" becomes "problem clients (was: e-commerce solutions)."</li>
+			<li>When responding to a post, delete all information that is not strictly necessary. This includes the evolt.org tag line and .sig files. Delete anything you're not specifically responding to.</li>
+			<li>When responding to a post, please make it clear who you're responding to. Not everyone is going to remember who the original sender was.</li>
+			<li>Don't post urban legends, virus alerts or humorous forwards. Please.</li>
+			<li>Don't post copyrighted material. If you think an article would be helpful to others on the list, provide a link.</li>
+			<li>You must ask permission if you want to forward or otherwise reproduce a post from a member of the list.</li>
+		</ol>
+		<div class="p100">
+			<p><a href="/mailman/listinfo/thelist">I understand and agree to these terms, and want to subscribe to thelist@lists.evolt.org</a></p>
+		</div>
+	</section>
+</div>
+<?
+include "assets/inc/99-footer.inc";
+?>
---
+++ web/current/index.php
@@ -0,0 +1,53 @@
+<?php
+// lists.evolt.org
+// Copyright (c) 1999-2013 evolt.org, William Anderson
+?>
+<?php
+include "assets/inc/00-functions.inc";
+$site_name = "lists.evolt.org";
+$page_title = "lists.evolt.org";
+$short_title = "Mailing Lists";
+$page_type = "page";
+include "assets/inc/10-header.inc";
+include "assets/inc/20-message.inc";
+?>
+<div class="wrapper">
+    <section id="content" class="full-width" role="main">
+    	<div class="p100">
+    		<h2><strong>evolt.org</strong> Mailing Lists</h2>
+			<p class="notice error"><strong>Pardon our dust!</strong> This iteration of <strong>evolt.org</strong> is under alpha test, containing content which has not yet been finalized.</p>
+			<p><strong>evolt.org</strong> hosts a number of mailing lists for the web development community.  Pick and choose which lists you want to be involved in.</p>
+		</div>
+		<ul class="user">
+			<li><a href="/mailman/listinfo/thelist"><strong>thelist</strong></a><br />
+				Become a part of the <strong>evolt.org</strong> community by joining "<strong>thelist</strong>", <strong>evolt.org</strong>'s primary discussion list for designers, developers and web managers working to make the Web a better place for all!  Ask questions, get answers.  Give answers, gain respect.  Become a part of the community!</li>
+		</ul>
+		<ul class="arrow">
+			<li><a href="/guidelines.php">View <strong>thelist</strong> information and guidelines</a></li>
+			<li><a href="/archive/">View <strong>thelist</strong> archives from April 1999 to present</a></li>
+		</ul>
+		<ul class="user">
+			<li><a href="/mailman/listinfo/theforum"><strong>theforum</strong></a><br />
+				Help create the future of <strong>evolt.org</strong> on "<strong>theforum</strong>" mailing list. Dedicated to making <strong>evolt.org</strong> a better place, discussions on this list include how to make thelist more useful to all members, improvements for the web site, how <strong>evolt.org</strong> should be structured, etc. Both short-term and long-term plans for <strong>evolt.org</strong> are discussed. If you're passionate about <strong>evolt.org</strong>, join now and help shape <strong>evolt.org</strong>'s future!</li>
+		</ul>
+		<ul class="arrow"><li><a href="/theforumarchive/">View <strong>thelist</strong> archives</a></li></ul>
+		<ul class="user">
+			<li><a href="/mailman/listinfo/thesite"><strong>thesite</strong></a><br />
+				The <strong>evolt.org</strong> mailing list dedicated to working on the back end of <strong>evolt.org</strong> where web developers contribute back to the community by making the <strong>evolt.org</strong> CMS a better product. Database folks, pixel wranglers, HTML/CSS monkeys, testers, documentation writers, developers - no matter what your skill set or skill level, you can help out!</li>
+		</ul>
+		<ul class="arrow"><li><a href="/thesitearchive/">View <strong>thelist</strong> archives</a></li></ul>
+		<ul class="user">
+			<li><a href="/mailman/listinfo/thechat"><strong>thechat</strong></a><br />
+				Chat it up on "<strong>thechat</strong>", <strong>evolt.org</strong>'s social mailing list useful for discussion about industry gossip, what hockey's about, cat photos, evolt get-togethers, and so on. For those "off-topic" conversations that don't belong on thelist!</li>
+		</ul>
+		<ul class="arrow"><li><a href="/thechatarchive/">View <strong>thelist</strong> archives</a></li></ul>
+		<div class="p100">
+			<h2>Tip Harvester</h2>
+			<p>The <strong>Tip Harvester</strong> collects the helpful tips from <strong>thelist</strong> that fellow evolters include in their mailing list posts to pay for off-topic posts.  This is a great source for specialized info where you will find nuggets of web genius.</p>
+			<p class="notice error"><strong>Pardon our dust!</strong> Unfortunately, the <strong>Tip Harvester</strong> is unavailable at present.</p>
+		</div>
+	</section>
+</div>
+<?
+include "assets/inc/99-footer.inc";
+?>
