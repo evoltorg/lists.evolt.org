441aa249fe372eefc00a3428ade5c9f4a101c94e
neuro <neuro@well.com>
2013-12-16 06:54:57 +0000

    4.0

--- web/current/index.php
+++ web/current/index.php
@@ -15,7 +15,7 @@ include "assets/inc/20-message.inc";
     <section id="content" class="full-width" role="main">
     	<div class="p100">
     		<h2><strong>evolt.org</strong> Mailing Lists</h2>
-			<p class="notice error"><strong>Pardon our dust!</strong> This iteration of <strong>evolt.org</strong> is under alpha test, containing content which has not yet been finalized.</p>
+			<!-- <p class="notice error"><strong>Pardon our dust!</strong> This iteration of <strong>evolt.org</strong> is under alpha test, containing content which has not yet been finalized.</p> -->
 			<p><strong>evolt.org</strong> hosts a number of mailing lists for the web development community.  Pick and choose which lists you want to be involved in.</p>
 		</div>
 		<ul class="user">
@@ -30,17 +30,17 @@ include "assets/inc/20-message.inc";
 			<li><a href="/mailman/listinfo/theforum"><strong>theforum</strong></a><br />
 				Help create the future of <strong>evolt.org</strong> on "<strong>theforum</strong>" mailing list. Dedicated to making <strong>evolt.org</strong> a better place, discussions on this list include how to make thelist more useful to all members, improvements for the web site, how <strong>evolt.org</strong> should be structured, etc. Both short-term and long-term plans for <strong>evolt.org</strong> are discussed. If you're passionate about <strong>evolt.org</strong>, join now and help shape <strong>evolt.org</strong>'s future!</li>
 		</ul>
-		<ul class="arrow"><li><a href="/theforumarchive/">View <strong>thelist</strong> archives</a></li></ul>
+		<ul class="arrow"><li><a href="/theforumarchive/">View <strong>theforum</strong> archives</a></li></ul>
 		<ul class="user">
 			<li><a href="/mailman/listinfo/thesite"><strong>thesite</strong></a><br />
 				The <strong>evolt.org</strong> mailing list dedicated to working on the back end of <strong>evolt.org</strong> where web developers contribute back to the community by making the <strong>evolt.org</strong> CMS a better product. Database folks, pixel wranglers, HTML/CSS monkeys, testers, documentation writers, developers - no matter what your skill set or skill level, you can help out!</li>
 		</ul>
-		<ul class="arrow"><li><a href="/thesitearchive/">View <strong>thelist</strong> archives</a></li></ul>
+		<ul class="arrow"><li><a href="/thesitearchive/">View <strong>thesite</strong> archives</a></li></ul>
 		<ul class="user">
 			<li><a href="/mailman/listinfo/thechat"><strong>thechat</strong></a><br />
 				Chat it up on "<strong>thechat</strong>", <strong>evolt.org</strong>'s social mailing list useful for discussion about industry gossip, what hockey's about, cat photos, evolt get-togethers, and so on. For those "off-topic" conversations that don't belong on thelist!</li>
 		</ul>
-		<ul class="arrow"><li><a href="/thechatarchive/">View <strong>thelist</strong> archives</a></li></ul>
+		<ul class="arrow"><li><a href="/thechatarchive/">View <strong>thechat</strong> archives</a></li></ul>
 		<div class="p100">
 			<h2>Tip Harvester</h2>
 			<p>The <strong>Tip Harvester</strong> collects the helpful tips from <strong>thelist</strong> that fellow evolters include in their mailing list posts to pay for off-topic posts.  This is a great source for specialized info where you will find nuggets of web genius.</p>
